ab845033a61e4712364dabdf302e2cc2c751c44a
inariyasannti <inariyasanntitokoyo@gmail.com>
2025-02-14 00:33:37 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  // GitHub Pages で公開後の manifest.plist の URL
+  const manifestUrl =
+    "https://username.github.io/my-ota-installer/manifest.plist";
+  const otaLink = `itms-services://?action=download-manifest&url=${encodeURIComponent(
+    manifestUrl
+  )}`;
+
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>iOSアプリ OTA インストーラ</h1>
+      <p>
+        下記のボタンをタップすると、iOSデバイスにアプリのインストールが開始されます。
+      </p>
+      <a
+        href={otaLink}
+        style={{
+          display: "inline-block",
+          padding: "10px 20px",
+          backgroundColor: "#007aff",
+          color: "#fff",
+          borderRadius: "5px",
+          textDecoration: "none",
+          fontSize: "16px",
+        }}
+      >
+        インストールする
+      </a>
+      <p style={{ marginTop: "20px", fontSize: "14px", color: "#666" }}>
+        ※ このページは iOS 端末でのみ動作確認してください。
+      </p>
+    </div>
+  );
+}
+
+export default App;
